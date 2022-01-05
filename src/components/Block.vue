4003799dc7fa6b0dfe6dc6451e0974700d60d069
Haris Basic <basic.haris@edu.fit.ba>
2022-01-05 02:35:31 +0100

    added

---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      click me
+  </div>
+</template>
+
+<script>
+import { TransitionGroup } from '@vue/runtime-dom'
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            rectionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        },this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.rectionTime += 10
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('end',this.rectionTime)
+        }
+    }
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #7111df;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>Your result is {{ score }} ms</h2>
+  <p>{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = 'Exelent'
+        } else if(this.score < 450) {
+            this.rank = 'Good'
+        } else if(this.score < 650) {
+            this.rank = 'That can be better'
+        } else if(this.score < 850) {
+            this.rank = 'OMG'
+        } else {
+            this.rank = 'Go home'
+        }
+    }
+}
+</script>
+
+<style>
+.rank {
+    font-size: 1.5em;
+    color: white;
+    font-weight: bold;
+}
+</style>
